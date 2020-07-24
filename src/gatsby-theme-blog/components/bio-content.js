b9c5b16b0096497b5fe53189a14eb3667e751c6c
chrissygonzalez <chrissygonzalez@gmail.com>
2020-07-24 18:49:24 -0400

    shadow blog footer

---
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react"
+
+/**
+ * Shadow me to add your own bio content
+ */
+
+const BioContent = () => (
+  <Fragment>
+      Hey hey hey hey
+  </Fragment>
+)
+
+export default BioContent
